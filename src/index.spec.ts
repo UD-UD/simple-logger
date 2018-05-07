53b68b15a764fbacc6af2c9d1fd097d3db61bc26
Abhas Bhattacharya <abhasbhattacharya2@gmail.com>
2018-05-07 19:02:09 +0530

    Add failing test case which shows the log

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,9 @@
-describe('test', () => {
-  it('teest1', () => {
-    expect(true).toEqual(true);
+import {SimpleLogger} from './index';
+
+describe('Simple logger', () => {
+  it('should be able to console.log message', () => {
+    const logger = new SimpleLogger('testModule');
+    logger.log('Hello world');
+    expect(1).toBe(2);
   })
 })
